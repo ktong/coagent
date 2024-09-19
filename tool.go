b4fb7d97beea0020c3679eef2263c305b73441fa
ktong <kuisong.tong@gmail.com>
2024-09-19 10:38:11 -0700

    simplify

--- tool.go
+++ tool.go
@@ -19,13 +19,9 @@ type Tool interface {
 	embedded.Tool
 }
 
-type functional[A, R any] interface {
-	func(context.Context, A) (R, error) | func(context.Context, *Thread, A) (R, error)
-}
-
 // Function calling allows you to describe functions to the Assistants API
 // and have it intelligently return the functions that need to be called along with their arguments.
-type Function[A, R any, FN functional[A, R]] struct {
+type Function[A, R any] struct {
 	embedded.Tool
 
 	// The name of the function to be called.
@@ -34,53 +30,35 @@ type Function[A, R any, FN functional[A, R]] struct {
 	// A description of what the function does, used by the model to choose when and how to call the function.
 	Description string
 	// The real function attached to the tool.
-	Function FN
+	Function func(context.Context, *Thread, A) (R, error)
 }
 
 // FunctionFor creates a function tool for either a function or an assistant.
-func FunctionFor[A, R any, S functional[A, R] | Assistant](s S,
-) Function[A, R, func(context.Context, *Thread, A) (R, error)] {
+func FunctionFor[A, R any, S func(context.Context, A) (R, error) | func(A) (R, error) |
+	func(context.Context, *Thread, A) (R, error) | func(*Thread, A) (R, error)](s S) Function[A, R] {
+	name := runtime.FuncForPC(reflect.ValueOf(s).Pointer()).Name()
+	name = name[strings.LastIndex(name, ".")+1:]
+
+	var function func(context.Context, *Thread, A) (R, error)
 	switch from := any(s).(type) {
-	case Assistant:
-		return Function[A, R, func(context.Context, *Thread, A) (R, error)]{
-			Name:        from.Name,
-			Description: from.Description,
-			Function: func(ctx context.Context, thread *Thread, argument A) (R, error) {
-				message, err := toMessage(argument)
-				if err != nil {
-					return *new(R), fmt.Errorf("convert argument to content: %w", err)
-				}
-				thread.Messages = append(thread.Messages, message)
-
-				err = from.Run(ctx, thread, message)
-				if err != nil {
-					return *new(R), fmt.Errorf("run assistant: %w", err)
-				}
-
-				return fromMessage[R](thread.Messages[len(thread.Messages)-1])
-			},
+	case func(context.Context, *Thread, A) (R, error):
+		function = from
+	case func(*Thread, A) (R, error):
+		function = func(_ context.Context, thread *Thread, argument A) (R, error) {
+			return from(thread, argument)
 		}
 	case func(context.Context, A) (R, error):
-		name := runtime.FuncForPC(reflect.ValueOf(from).Pointer()).Name()
-		name = name[strings.LastIndex(name, ".")+1:]
-
-		return Function[A, R, func(context.Context, *Thread, A) (R, error)]{
-			Name: name,
-			Function: func(ctx context.Context, _ *Thread, argument A) (R, error) {
-				return from(ctx, argument)
-			},
+		function = func(ctx context.Context, _ *Thread, argument A) (R, error) {
+			return from(ctx, argument)
 		}
-	case func(context.Context, *Thread, A) (R, error):
-		name := runtime.FuncForPC(reflect.ValueOf(from).Pointer()).Name()
-		name = name[strings.LastIndex(name, ".")+1:]
-
-		return Function[A, R, func(context.Context, *Thread, A) (R, error)]{
-			Name:     name,
-			Function: from,
+	case func(A) (R, error):
+		function = func(_ context.Context, _ *Thread, argument A) (R, error) {
+			return from(argument)
 		}
 	default:
-		return Function[A, R, func(context.Context, *Thread, A) (R, error)]{} // Should not happen.
 	}
+
+	return Function[A, R]{Name: name, Function: function}
 }
 
 // Below are workarounds for allowing the generic type to be used in the function call.
@@ -92,7 +70,7 @@ type FunctionSchema struct {
 	Parameter   schema.Schema
 }
 
-func (f Function[A, R, FN]) Schema() (FunctionSchema, error) {
+func (f Function[A, R]) Schema() (FunctionSchema, error) {
 	parameterSchema, err := schema.For[A]()
 	if err != nil {
 		return FunctionSchema{}, fmt.Errorf("generate function schema: %w", err)
@@ -105,11 +83,11 @@ func (f Function[A, R, FN]) Schema() (FunctionSchema, error) {
 	}, nil
 }
 
-func (f Function[A, R, FN]) ID() string {
+func (f Function[A, R]) ID() string {
 	return f.Name
 }
 
-func (f Function[A, R, FN]) Call(ctx context.Context, thread *Thread, a string) (Message, error) {
+func (f Function[A, R]) Call(ctx context.Context, thread *Thread, a string) (Message, error) {
 	var argument A
 	if err := json.Unmarshal([]byte(a), &argument); err != nil {
 		return Message{}, fmt.Errorf("unmarshal function call arguments: %w", err)
@@ -119,12 +97,7 @@ func (f Function[A, R, FN]) Call(ctx context.Context, thread *Thread, a string) 
 		result R
 		err    error
 	)
-	switch fn := any(f.Function).(type) {
-	case func(context.Context, A) (R, error):
-		result, err = fn(ctx, argument)
-	case func(context.Context, *Thread, A) (R, error):
-		result, err = fn(ctx, thread, argument)
-	}
+	result, err = f.Function(ctx, thread, argument)
 	if err != nil {
 		return Message{}, fmt.Errorf("call function: %w", err)
 	}
